35117f50d02010b0a6d0cd63c429f1680b4c9995
Mark Roy <markproy3@gmail.com>
2018-09-29 06:42:42 -0400

    removed individual lab folders

--- lab1/package_images.bat
+++
@@ -1,4 +0,0 @@
-python im2rec.py --list --recursive --train-ratio 0.75 nabirds_sample2 ../images/
-python im2rec.py --resize 256 nabirds_sample2 ../images/
-
-aws s3 cp nabirds_sample2_train.rec s3://<bucket>/nabirds_sample2_train.rec
