7b1f13e133799e192ed3008ef03c115dfc2d16b8
Mark Roy <roymark@amazon.com>
2018-10-26 14:56:43 -0400

    add user suffix

--- labs/lab3/test_all.sh
+++ labs/lab3/test_all.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# this script should be run in a terminal window inside your SageMaker notebook
+# that was created as part of the workshop.
+
+if [ $# -lt 1 ]
+then
+  echo Pass the user suffix, as in:
+  echo   bash test_all.sh 02
+  exit 1
+fi
+
+export ENDPOINT_NAME=nabirds-species-identifier-$1
+
 python test_direct_sample.py ../../test_images/eastern-bluebird.jpg
 python test_direct_sample.py ../../test_images/northern-cardinal.jpg
 python test_direct_sample.py ../../test_images/american-goldfinch.jpg
