cab79f7ad53671821b79e156a81beed05cfa6f70
Mark Roy <roymark@amazon.com>
2018-11-16 12:07:22 -0500

    us-east-1 catch

--- labs/lab0/notebooks_state.sh
+++ labs/lab0/notebooks_state.sh
@@ -5,8 +5,12 @@
 # second argument is an AWS credentials profile that corresponds to a configured
 # entry in the AWS credentials configuration file to refer to a specific IAM user.
 
-# ./notebooks_state.sh start deeplens-1
-# ./notebooks_state.sh stop deeeplens-3
+# ./notebooks_state.sh start deeplens-1 us-east-1
+# ./notebooks_state.sh stop deeplens-3 us-east-1
+
+if [ $# -lt 3 ]; then
+  echo Pass in state, account, and region, as in: ./notebooks_state.sh start deeplens-1 us-east-1
+fi
 
 if [ $1 == "start" ]; then
    Command="start-notebook-instance"
@@ -17,14 +21,15 @@ else
 fi
 
 Profile=$2
+Region=$3
 
 echo $Profile
 echo $Command
 
 set -x
-aws sagemaker --profile ${Profile} --region us-east-1 list-notebook-instances \
+aws sagemaker --profile ${Profile} --region ${Region} list-notebook-instances \
   --status-equals ${Selection} --output text | cut -f 7 | \
-  xargs -I{} aws sagemaker --profile ${Profile} --region us-east-1 $Command --notebook-instance-name {}
+  xargs -I{} aws sagemaker --profile ${Profile} --region ${Region} $Command --notebook-instance-name {}
 
 #--query "NotebookInstances[?NotebookInstanceStatus=='${Selection}'].NotebookInstanceName"
 #BirdClassificationWorkshop02-01	BirdClassificationWorkshop01-08
