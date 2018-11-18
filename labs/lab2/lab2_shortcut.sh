f4b3d610dc96d384725081c34e5b5ed583d5328d
Mark Roy <roymark@amazon.com>
2018-11-18 16:17:03 -0500

    update

--- labs/lab2/lab2_shortcut.sh
+++ labs/lab2/lab2_shortcut.sh
@@ -31,6 +31,7 @@ if [[ ${REGION} == "us-east-1" ]]; then
   TRAINING_IMAGE=811284229777.dkr.ecr.us-east-1.amazonaws.com/image-classification:1
 fi
 
+INSTANCE_TYPE=ml.p3.2xlarge
 PREFIX=
 TRAINING_JOB_NAME=birds-`date '+%Y-%m-%d-%H-%M-%S'`
 
@@ -43,7 +44,7 @@ aws --profile $PROFILE --region $REGION sagemaker create-training-job \
   --role-arn $EXECUTION_ROLE_ARN \
   --training-job-name $TRAINING_JOB_NAME \
   --algorithm-specification TrainingImage=$TRAINING_IMAGE,TrainingInputMode=File \
-  --resource-config InstanceCount=1,InstanceType=ml.p3.2xlarge,VolumeSizeInGB=50 \
+  --resource-config InstanceCount=1,InstanceType=$INSTANCE_TYPE,VolumeSizeInGB=50 \
   --input-data-config ChannelName=train,DataSource=$TRAIN_SOURCE,ContentType=application/x-recordio,CompressionType=None,RecordWrapperType=None ChannelName=validation,DataSource=$VAL_SOURCE,ContentType=application/x-recordio,CompressionType=None,RecordWrapperType=None \
   --output-data-config S3OutputPath=$BUCKET$PREFIX \
   --hyper-parameters '{"augmentation_type": "crop", "beta_1": "0.9", "beta_2": "0.999","checkpoint_frequency": "25", "epochs": "25", "eps": "1e-8","gamma": "0.9", "image_shape": "3,224,224","learning_rate": "0.1", "lr_scheduler_factor": "0.1","mini_batch_size": "24", "momentum": "0.9", "multi_label": "0","num_classes": "4", "num_layers": "152", "num_training_samples": "322","optimizer": "sgd", "precision_dtype": "float32", "top_k": "2","use_pretrained_model": "0", "use_weighted_loss": "0","weight_decay": "0.0001"}' \
